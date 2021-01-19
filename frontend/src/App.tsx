cabe0d3870128e294aeb65d410ba6e746180982f
acomanzo <ansonih17@gmail.com>
2021-01-19 17:50:02 -0500

    Created new component for officers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Main from './components/main/Main';
-
+import { Slider } from './components/events/Slider';
+import { Officers } from './components/officers/Officers';
 
 
 function App() {
   return (
-  <Main />
+    <div>
+      <Main />
+      <Slider />
+      <Officers />
+    </div>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
+import './styles/slider.css';
+import './styles/officers.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
