79bd514a74de85924e6c9984b7e892254baf8d77
acomanzo <ansonih17@gmail.com>
2021-01-25 15:33:46 -0500

    adjusted the look of the officers component

--- frontend/src/components/officers/Officers.tsx
+++ frontend/src/components/officers/Officers.tsx
@@ -1,32 +1,84 @@
 import React, { useState } from 'react';
 
 export const Officers: React.FC = () => {
-    let officers: JSX.Element[] = [
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
-        <OfficerCard />,
+
+    const eboard: JSX.Element[] = [
         <OfficerCard />,
         <OfficerCard />,
         <OfficerCard />,
         <OfficerCard />
     ];
 
+    const jboard: JSX.Element[] = [
+        <OfficerCard />,
+        <OfficerCard />,
+        <OfficerCard />,
+        <OfficerCard />
+    ];
+
+    const dboard: JSX.Element[] = [
+        <OfficerCard />,
+        <OfficerCard />
+    ];
+
+    // hook with an initial state as the e board
+    const [selectedOfficersGroup, setSelectedOfficersGroup] = useState(eboard);
+
+    // hook to keep track of the active group that helps in toggleOfficerGroup()
+    const [selectedOfficersGroupName, setSelectedOfficersGroupName] = useState('eboard');
+
+    function toggleOfficerGroup(event: any) {
+        const list: NodeListOf<HTMLElement> = document.querySelectorAll('ul.officer-group-toggle-container li');
+        switch (selectedOfficersGroupName) {
+            case 'eboard':
+                list[0].classList.remove('active');
+                break;
+            case 'jboard':
+                list[1].classList.remove('active');
+                break;
+            case 'dboard':
+                list[2].classList.remove('active');
+                break;
+            default:
+                list[0].classList.remove('active');
+                break;
+        }
+
+        event.target.classList.add('active');
+        switch (event.target.innerText) {
+            case 'E-Board':
+                setSelectedOfficersGroup(eboard);
+                setSelectedOfficersGroupName('eboard');
+                break;
+            case 'J-Board':
+                setSelectedOfficersGroup(jboard);
+                setSelectedOfficersGroupName('jboard');
+                break;
+            case 'D-Board':
+                setSelectedOfficersGroup(dboard);
+                setSelectedOfficersGroupName('dboard');
+                break;
+            default:
+                setSelectedOfficersGroup(eboard);
+                setSelectedOfficersGroupName('eboard');
+                break;
+        }
+    }
+
     return (
         <div>
-            <h1 style={{marginLeft: "2%"}}>Meet the officers</h1>
+            <div className="officers-header">
+                <h1 style={{marginLeft: "2%"}}>Meet the...</h1>
+                <ul className="officer-group-toggle-container">
+                    <li className="officer-group-toggle active" onClick={toggleOfficerGroup}>E-Board</li>
+                    <li className="officer-group-toggle" onClick={(e) => toggleOfficerGroup(e)}>J-Board</li>
+                    <li className="officer-group-toggle" onClick={toggleOfficerGroup}>D-Board</li>
+                </ul>
+            </div>
+            <div className="highlight"></div>
             <div className="officers-container">
                 {
-                    officers.map((item: JSX.Element, index: number) => {
+                    selectedOfficersGroup.map((item: JSX.Element, index: number) => {
                         return (
                             <div key={index} style={{margin: "2%"}}>{item}</div>
                         )
