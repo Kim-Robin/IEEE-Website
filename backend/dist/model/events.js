6416e0065a26661e6b3f2e4ede4e1a9bafca49a1
login <robin1265@gmail.com>
2021-02-10 21:49:23 -0500

    package.json updated

---
+++ backend/dist/model/events.js
@@ -0,0 +1,21 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.eventModel = void 0;
+const db_1 = require("../database/db");
+const getAllEvents = () => __awaiter(void 0, void 0, void 0, function* () {
+    const values = yield db_1.dbQuery(`SELECT * FROM Events`);
+    return values;
+});
+exports.eventModel = {
+    getAllEvents
+};
+//# sourceMappingURL=events.js.map
---
+++ backend/dist/routes/event.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const express_1 = __importDefault(require("express"));
+const events_1 = require("../model/events");
+const router = express_1.default.Router();
+/* event section endpoint*/
+router.get('/events/past', (req, res, next) => {
+    res.send("past event");
+});
+router.get('/events/future', (req, res, next) => {
+    res.send("future event");
+});
+router.get('/events', (req, res) => {
+    events_1.eventModel.getAllEvents()
+        .then(events => {
+        res.json(events);
+    })
+        .catch(err => res.status(500).json({
+        err: err.message
+    }));
+});
+module.exports = router;
+//# sourceMappingURL=event.js.map
