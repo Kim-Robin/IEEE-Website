1c5e657e921b52e3f21bd66085a647bbb33d4c25
login <robin1265@gmail.com>
2021-02-10 21:05:28 -0500

    minor tweak

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -3,17 +3,17 @@ import sqlite3 from 'sqlite3';
 
 const DATABASE_FILE: string = "./IEEE_Info_DB.db";
 
-let db = new sqlite3.Database(DATABASE_FILE, (err)=>{
-    if(err){
-        return console.error(err.message);
-    }
-    console.log("Connection to the in-memory SQL");
-});
 
 
 
 export const dbQuery = async (query: string, params?: any[]) =>{
 
+     let db = new sqlite3.Database(DATABASE_FILE, (err)=>{
+         if(err){
+             return console.error(err.message);
+         }
+         console.log("Connection to the in-memory SQL");
+     });
      const process = new Promise<any[]>((resolve, reject) =>{
 
         db.all(query, params, (err, rows) =>{
