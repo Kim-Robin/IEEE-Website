64a699e94f44800b47f93570adb1d2ff4d3520e5
acomanzo <ansonih17@gmail.com>
2021-03-06 01:33:52 -0500

    removed contact button and added on click to about

--- frontend/src/components/main/Navbar.tsx
+++ frontend/src/components/main/Navbar.tsx
@@ -67,32 +67,26 @@ const Navbar: React.FC<Props> = ({className}) =>{
             <div className={className}>
                 <ul className="nav-list">
                     <li className="nav-list-item" onClick={logoHandle}>
+                        <a href="#about-component" >About</a>
+                    </li>
+                    <li className="nav-list-item" onClick={logoHandle}>
                         <a href="#events-component"  >Event</a>
                     </li>
-                    <li className="nav-list-item">
+                    <li className="nav-list-item" onClick={logoHandle}>
                         <a href="#officers-component" >Members</a>
-                    </li>
-                    <li className="nav-list-item">
-                        <a href="" >About</a>
-                    </li>
-                    <li className="nav-list-item">
-                        <a href="" >Contact</a>
                     </li>
                 </ul> 
             </div>
             <div id="overlay">
                 <ul className="overlay-content">
                     <li className="overlay-nav-list-item">
+                        <a href="#about-component" onClick={logoHandle} >About</a>
+                    </li>
+                    <li className="overlay-nav-list-item">
                         <a href="#events-component" onClick={logoHandle} >Event</a>
                     </li>
                     <li className="overlay-nav-list-item">
                         <a href="#officers-component" onClick={logoHandle} >Members</a>
-                    </li>
-                    <li className="overlay-nav-list-item">
-                        <a href="" onClick={logoHandle} >About</a>
-                    </li>
-                    <li className="overlay-nav-list-item">
-                        <a href="" onClick={logoHandle} >Contact</a>
                     </li>
                 </ul>
             </div>
